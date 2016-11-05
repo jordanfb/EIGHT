1077108764b5e681a31ceaa0e47dd64066c26c77
Jordan <jmanfb@gmail.com>
2016-11-05 12:36:04 -0400

    Basic structure and form of the game

---
+++ src/game.lua
@@ -0,0 +1,88 @@
+
+
+require "class"
+
+Game = class()
+
+-- _init, load, draw, update(dt), keypressed, keyreleased, mousepressed, mousereleased, resize, (drawUnder, updateUnder)
+
+function Game:_init()
+	-- these are for draw stacks:
+	self.drawUnder = false
+	self.updateUnder = false
+
+	-- here are the actual variables
+	self.drawFPS = true
+
+	self.screenStack = {}
+end
+
+function Game:load(args)
+	--
+end
+
+function Game:draw()
+	if (self.drawFPS) then
+		love.graphics.setColor(255, 0, 0)
+		love.graphics.print("FPS: "..love.timer.getFPS(), 10, 10)
+	end
+
+	local thingsToDraw = 1 -- this will become the index of the lowest item to draw
+	for i = #self.screenStack, 1, -1 do
+		thingsToDraw = i
+		if not self.screenStack[i].drawUnder then
+			break
+		end
+	end
+	-- this is so that the things earlier in the screen stack get drawn first, so that things like pause menus get drawn on top.
+	for i = thingsToDraw, #self.screenStack, 1 do
+		self.screenStack[i]:draw()
+	end
+end
+
+function Game:update(dt)
+	self.network:update(dt)
+	for i = #self.screenStack, 1, -1 do
+		self.screenStack[i]:update(dt)
+		if not self.screenStack[i].updateUnder then
+			break
+		end
+	end
+end
+
+function Game:popScreenStack()
+	self.screenStack[#self.screenStack]:leave()
+	self.screenStack[#self.screenStack] = nil
+	self.screenStack[#self.screenStack]:load()
+end
+
+function Game:addToScreenStack(newScreen)
+	self.screenStack[#self.screenStack+1] = newScreen
+	newScreen:load()
+end
+
+function Game:resize(w, h)
+	for i = 1, #self.screenStack, 1 do
+		self.screenStack[i]:resize(w, h)
+	end
+end
+
+function Game:keypressed(key, unicode)
+	self.screenStack[#self.screenStack]:keypressed(key, unicode)
+end
+
+function Game:keyreleased(key, unicode)
+	self.screenStack[#self.screenStack]:keyreleased(key, unicode)
+end
+
+function Game:mousepressed(x, y, button)
+	self.screenStack[#self.screenStack]:mousepressed(x, y, button)
+end
+
+function Game:mousereleased(x, y, button)
+	self.screenStack[#self.screenStack]:mousereleased(x, y, button)
+end
+
+function Game:quit()
+	self.network:quit()
+end
---
+++ src/main.lua
@@ -0,0 +1,51 @@
+io.stdout:setvbuf("no") -- this is so that sublime will print things when they come (rather than buffering).
+
+
+
+require "game"
+require "class"
+
+
+local game = Game()
+
+
+function love.load(args)
+	game:load(args)
+	--local width, height = 512, 256
+	local width, height = 800, 600
+	love.window.setMode(width, height, {resizable = true})
+	-- not much here
+end
+
+function love.resize(w, h)
+	game:resize(w, h)
+end
+
+function love.draw()
+	game:draw()
+end
+
+function love.update(dt)
+	--print(1/dt) -- the framerate, I think.
+	game:update(dt)
+end
+
+function love.keypressed(key, unicode)
+	game:keypressed(key, unicode)
+end
+
+function love.keyreleased(key, unicode)
+	game:keyreleased(key, unicode)
+end
+
+function love.mousepressed(x, y, button)
+	game:mousepressed(x, y, button)
+end
+
+function love.mousereleased(x, y, button)
+	game:mousereleased(x, y, button)
+end
+
+function love.quit()
+	game:quit()
+end
