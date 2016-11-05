add0fc5ab4621f2cbffb2ebb03e65de24e5ad803
Jordan Faas-Bush <jmanfb@gmail.com>
2016-11-05 17:43:09 -0400

    added attacks class that should mostly work except for double damage

--- src/attacks.lua
+++ src/attacks.lua
@@ -12,12 +12,14 @@ function Attacks:_init(level, players)
 	self.level = level
 	self.players = players
 	self.attacks = {}
-	-- {x, y, width, height, color, damage?, facing}
+	-- {x, y, width, height, color, damage?, facing, time}
+	self.firstAttack = 1
 end
 
 function Attacks:checkCollisions(player, playerX, playerY, playerWidth, playerHeight)
-	-- {x, y, width, height, color, damage?}
-	for k, attack in pairs(self.attacks) do
+	-- {x, y, width, height, color, damage?, facing, time}
+	for i = self.firstAttack, #self.attacks, 1 do
+		local attack = self.attacks[i]
 		if playerX + playerWidth > attack[1] and playerX < attack[1]+attack[3] then
 			if playerY + playerHeight > attack[2] and playerY < attack[2]+attack[4] then
 				-- then it hits, so deal the damage.
@@ -32,9 +34,23 @@ function Attacks:checkCollisions(player, playerX, playerY, playerWidth, playerHe
 	end
 end
 
+function Attacks:update(dt)
+	for i = self.firstAttack, #self.attacks, 1 do
+		local attack = self.attacks[i]
+		attack[8] = attack[8]-dt
+		if attack[8] <= 0 then
+			self.firstAttack = self.firstAttack + 1 -- move up the line. fuck efficiency.
+		end
+	end
+end
+
 function Attacks:draw()
 	love.graphics.setColor(0, 0, 255)
-	for k, attack in pairs(self.attacks) do
+	for i = self.firstAttack, #self.attacks, 1 do
 		love.graphics.rectangle("fill", attack[1], attack[2], attack[3], attack[4])
 	end
+end
+
+function Attacks:newAttack(x, y, width, height, color, damage, facing, time)
+	self.attacks[#self.attacks+1] = {x, y, width, height, color, damage, facing, time}
 end
