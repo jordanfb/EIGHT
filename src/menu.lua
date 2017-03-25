85dc150e507680f497d26fa92319868c4538dfb8
Jordan <jmanfb@gmail.com>
2017-03-24 20:36:41 -0400

    Edited the settings selection so that it should work nicer

--- src/menu.lua
+++ src/menu.lua
@@ -8,6 +8,7 @@ function Menu:_init(game, buttons, x, y, width, height, selection)
 	self.game = game
 	self.controllingInput = 0 -- the input that is allowed to edit. 0 can be controlled by anyone.
 	self.buttons = {}
+	self.numButtons = 0
 	self:importFromButtons(buttons)
 	self.buttonBackup = {}
 	self:makeDefaultsBackup()
@@ -28,6 +29,7 @@ end
 function Menu:makeDefaultsBackup()
 	for i = 1, #self.buttons do
 		self.buttonBackup[self.buttons[i].message] = self.game.gameSettings[self.buttons[i].message]
+		self.numButtons = self.numButtons + 1
 	end
 end
 
@@ -159,19 +161,52 @@ function Menu:inputMade(inputNum, input, pressValue)
 			-- self.selectionToggle = false
 		end
 		if input == "up" or input == "menuup" then
-			self.selection = self.selection - self.numPerHorizontal
+			if self.selection - self.numPerHorizontal < 0 then
+				-- goes off the top
+				if self.selection > (self.numButtons-1) % self.numPerHorizontal then
+					self.selection = self.numButtons-1 - (self.numButtons-1)%self.numPerHorizontal - self.numPerHorizontal + self.selection % self.numPerHorizontal
+				else
+					-- print("hello")
+					self.selection = math.floor((self.numButtons-1)/self.numPerHorizontal)*self.numPerHorizontal + self.selection % self.numPerHorizontal
+				end
+			else
+				self.selection = self.selection - self.numPerHorizontal
+			end
 		elseif input == "down" or input == "menudown" then
-			self.selection = self.selection + self.numPerHorizontal
+			if self.selection + self.numPerHorizontal >= self.numButtons then
+				self.selection = self.selection % self.numPerHorizontal -- goes off the top
+			else
+				self.selection = self.selection + self.numPerHorizontal
+			end
 		elseif input == "left" or input == "lookleft" or input == "menuleft" then
-			self.selection = self.selection - 1
+			if self.selection % self.numPerHorizontal == 0 then
+				-- move to the right of the row
+				self.selection = self.selection + self.numPerHorizontal-1
+				if self.selection > self.numButtons then
+					self.selection = self.numButtons - 1 -- because you went off the end
+				end
+			else
+				self.selection = self.selection - 1
+			end
 		elseif input == "right" or input == "lookright" or input == "menuright" then
-			self.selection = self.selection + 1
+		-- 	print("Num "..self.numButtons)
+		-- 	print(math.floor((self.numButtons-1)/self.numPerHorizontal)*self.numPerHorizontal)
+			if self.selection % self.numPerHorizontal == self.numPerHorizontal - 1 then
+				self.selection = self.selection - self.numPerHorizontal + 1
+			else
+				if self.selection >= math.floor((self.numButtons-1)/self.numPerHorizontal)*self.numPerHorizontal then
+					-- it's on the last row, so set yourself to the left side of that.
+					self.selection = math.floor((self.numButtons-1)/self.numPerHorizontal)*self.numPerHorizontal
+				else
+					self.selection = self.selection + 1
+				end
+			end
 		end
 	end
-	if self.selection >= #self.buttons then
-		self.selection = self.selection % self.numPerHorizontal
-	end
-	if self.selection < 0 then
-		self.selection = #self.buttons+self.selection
-	end
+	-- if self.selection >= #self.buttons then
+	-- 	self.selection = self.selection % self.numPerHorizontal
+	-- end
+	-- if self.selection < 0 then
+	-- 	self.selection = #self.buttons+self.selection
+	-- end
 end
